b5bd468f6300ad1036d48a2767e9be0b9620ae78
yashparmar1073 <160565881+yashparmar1073@users.noreply.github.com>
2024-10-18 17:09:21 +0530

    Update fetch_file_name.py

--- fetch_file_name.py
+++ fetch_file_name.py
@@ -1,34 +1,45 @@
 import re
+import argparse
 
-# Specify the file path
-file_path = "/Users/yash.parmar/Documents/Perl/logs.txt"
+parser = argparse.ArgumentParser(description='My Python script')
+parser.add_argument('--input-file', type=str, required=True, help='Input file')
+args = parser.parse_args()
 
-# Read the file content
-with open(file_path, 'r') as file:
-    file_content = file.read()
+# Read the contents of the input file
+with open(args.input_file, 'r') as f:
+    contents = f.read()
 
-# Extract the "name" field key-value pair using a regular expression
-name  = r"name\s*=>\s*'([^']*)'"
-name = re.search(name, file_content)
+print(f'Input file contents: {contents}')
 
-category = r"category\s*=>\s*'([^']*)'"
-category = re.search(category, file_content)
+# # Specify the file path
+# file_path = "/Users/yash.parmar/Documents/Perl/logs.txt"
 
-team = r"team\s*=>\s*'([^']*)'"
-team = re.search(team, file_content)
+# # Read the file content
+# with open(file_path, 'r') as file:
+#     file_content = file.read()
 
-if category:
-    print(category.group(1).strip())
-else:
-    print("No  field found in the file.")
+# # Extract the "name" field key-value pair using a regular expression
+# name  = r"name\s*=>\s*'([^']*)'"
+# name = re.search(name, file_content)
+
+# category = r"category\s*=>\s*'([^']*)'"
+# category = re.search(category, file_content)
+
+# team = r"team\s*=>\s*'([^']*)'"
+# team = re.search(team, file_content)
+
+# if category:
+#     print(category.group(1).strip())
+# else:
+#     print("No  field found in the file.")
 
 
-if name:
-    print(name.group(1).strip())
-else:
-    print("No  field found in the file.")
+# if name:
+#     print(name.group(1).strip())
+# else:
+#     print("No  field found in the file.")
 
-if team:
-    print(team.group(1).strip())
-else:
-    print("No  field found in the file.")
+# if team:
+#     print(team.group(1).strip())
+# else:
+#     print("No  field found in the file.")
