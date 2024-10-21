8c6f9d5d24b0a81ca96d3eeedde732b970535231
yashparmar1073 <160565881+yashparmar1073@users.noreply.github.com>
2024-10-21 17:21:40 +0530

    Update add_testcase.py

--- add_testcase.py
+++ add_testcase.py
@@ -33,7 +33,7 @@ except subprocess.CalledProcessError as e:
 
 
 output = output.strip()  
-file_path=f'{output} +'/output.pk'
+file_path=f"{output}" + "/output.pk"
 with open(file_path, "rb") as file:
     loaded_dict = pickle.load(file)
 
