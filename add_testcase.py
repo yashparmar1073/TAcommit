be9330547d006cffb897980d8a7de07da8e46726
yashparmar1073 <160565881+yashparmar1073@users.noreply.github.com>
2024-10-21 17:32:22 +0530

    Update add_testcase.py

--- add_testcase.py
+++ add_testcase.py
@@ -1,26 +1,26 @@
-#import requests
+import requests
 import pickle
 import subprocess
 
-# url = 'https://core.heimdall.c03.pit.els.sophos/results/testcase_mgmt.php?nav=Testcases&subnav=Management'
-# headers = {
-#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-#     'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-#     'Cache-Control': 'max-age=0',
-#     'Connection': 'keep-alive',
-#     'Content-Type': 'application/x-www-form-urlencoded',
-#     'Origin': 'https://core.heimdall.c03.pit.els.sophos',
-#     'Referer': 'https://core.heimdall.c03.pit.els.sophos/results/testcase_mgmt.php?nav=Testcases&subnav=Management',
-#     'Sec-Fetch-Dest': 'document',
-#     'Sec-Fetch-Mode': 'navigate',
-#     'Sec-Fetch-Site': 'same-origin',
-#     'Sec-Fetch-User': '?1',
-#     'Upgrade-Insecure-Requests': '1',
-#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
-#     'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
-#     'sec-ch-ua-mobile': '?0',
-#     'sec-ch-ua-platform': '"macOS"'
-# }
+url = 'https://core.heimdall.c03.pit.els.sophos/results/testcase_mgmt.php?nav=Testcases&subnav=Management'
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'https://core.heimdall.c03.pit.els.sophos',
+    'Referer': 'https://core.heimdall.c03.pit.els.sophos/results/testcase_mgmt.php?nav=Testcases&subnav=Management',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"'
+}
 
 #current location is used to open the file
 command = "pwd"
@@ -39,20 +39,20 @@ with open(file_path, "rb") as file:
     loaded_dict = pickle.load(file)
 
 print(loaded_dict)
-# data = {
-#     'id': '',
-#     'product': 'COP',
-#     'product_add': '',
-#     'category': 'Administration',
-#     'category_add': '',
-#     'testcase': 'aaaaayash',
-#     'team': 'Base',
-#     'effort': '0',
-#     'notes': '',
-#     'OK': 'OK'
-# }
+data = {
+    'id': '',
+    'product': 'COP',
+    'product_add': '',
+    'category': 'Administration',
+    'category_add': '',
+    'testcase': 'aaaaayash',
+    'team': 'Base',
+    'effort': '0',
+    'notes': '',
+    'OK': 'OK'
+}
 
-# response = requests.post(url, headers=headers, data=data, verify=False)
+response = requests.post(url, headers=headers, data=data, verify=False)
 
-# print(response.status_code)
+print(response.status_code)
 #print(response.text)
